6efdfcd1f498900bcaf92735ed4d13d554e69534
Nabil Khalid <holanabilkhalid@gmail.com>
2023-08-29 00:19:32 +0800

    Modify and add methods of gameboard class

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,24 +1,37 @@
 export default class Gameboard {
     constructor() {
-        this.board = Array.from({ length: 8 }, () => new Array(8).fill(''));
+        this.board = Array.from({ length: 10 }, () => new Array(10).fill(''));
         this.ships = [];
     }
 
+    isShipPlacementOutOfBound(startIndex, shipLength) {
+        const endIndex = startIndex + shipLength - 1;
+        return endIndex < this.board.length;
+    }
+
     placeShip(shipObj, coorArr, orientation) {
-        const [row, column] = coorArr;
         this.ships.push(shipObj);
-        for (let i = column; i <= column + shipObj.shipLength; i++) {
-            this.board[row][i] = shipObj.name.chartAt(0);
+        const [row, column] = coorArr;
+        switch (orientation) {
+            case 'horizontal':
+                for (let i = column; i < column + shipObj.shipLength; i++) {
+                    this.board[row][i] = shipObj.name.charAt(0);
+                }
+                break;
+            default:
+                for (let i = row; i < row + shipObj.shipLength; i++) {
+                    this.board[i][column] = shipObj.name.charAt(0);
+                }
         }
     }
 
     // Enemy attack
-    receiveAttack(enemycoorArr, opponentObj) {
+    receiveAttack(enemycoorArr, opponentShip) {
         const [row, column] = enemycoorArr;
         const { board } = this;
         if (board[row][column] !== '') {
             board[row][column] += ' + Hit';
-            opponentObj.hit();
+            opponentShip.hit();
             return true;
         }
         board[row][column] += 'MISS';
