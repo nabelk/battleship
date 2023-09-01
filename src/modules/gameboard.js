275d04b28c3352ef8c43f24df9c293bed22534d9
Nabil Khalid <holanabilkhalid@gmail.com>
2023-09-02 05:18:30 +0800

    feat: Add ship placement validation & refactor: Fix existing code

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,26 +1,77 @@
 export default class Gameboard {
     constructor() {
-        this.board = Array.from({ length: 10 }, () => new Array(10).fill(''));
+        this.board = Array.from({ length: 10 }, () =>
+            new Array(10).fill({ text: '', hasShip: false, isHit: false })
+        );
         this.ships = [];
     }
 
-    isShipPlacementOutOfBound(startIndex, shipLength) {
+    isShipPlacementInBound(startIndex, shipLength) {
         const endIndex = startIndex + shipLength - 1;
         return endIndex < this.board.length;
+    }
+
+    isCellHasShip(orientation, shipLength, coor) {
+        const [row, column] = coor;
+        switch (orientation) {
+            case 'horizontal':
+                for (let i = column; i < column + shipLength; i++) {
+                    if (this.board[row][i].hasShip) {
+                        return true;
+                    }
+                }
+                break;
+            default:
+                for (let i = row; i < row + shipLength; i++) {
+                    if (this.board[i][column].hasShip) {
+                        return true;
+                    }
+                }
+                break;
+        }
+        return false;
+    }
+
+    checkCellHasShipAndShipPlacementInBound(shipLength, orientation, coor) {
+        const [row, column] = coor;
+
+        if (orientation === 'horizontal') {
+            if (!this.isShipPlacementInBound(column, shipLength)) {
+                return false;
+            }
+            if (this.isCellHasShip(orientation, shipLength, coor)) {
+                return false;
+            }
+        } else {
+            if (!this.isShipPlacementInBound(row, shipLength)) {
+                return false;
+            }
+
+            if (this.isCellHasShip(orientation, shipLength, coor)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     placeShip(shipObj, coorArr, orientation) {
         this.ships.push(shipObj);
         const [row, column] = coorArr;
+        const info = {
+            text: shipObj.name,
+            hasShip: true,
+            isHit: false,
+        };
         switch (orientation) {
             case 'horizontal':
                 for (let i = column; i < column + shipObj.shipLength; i++) {
-                    this.board[row][i] = shipObj.name.charAt(0);
+                    this.board[row][i] = info;
                 }
                 break;
             default:
                 for (let i = row; i < row + shipObj.shipLength; i++) {
-                    this.board[i][column] = shipObj.name.charAt(0);
+                    this.board[i][column] = info;
                 }
         }
     }
@@ -32,7 +83,7 @@ export default class Gameboard {
         );
         const [row, column] = enemycoorArr;
         const { board } = this;
-        if (board[row][column] !== '') {
+        if (board[row][column].text !== '') {
             board[row][column] += ' + Hit';
             findShipObj.hit();
             return true;
