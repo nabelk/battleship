3daf0d9ea5699b310d6fb282785a93094819b27d
Nabil Khalid <holanabilkhalid@gmail.com>
2023-09-17 01:46:33 +0800

    feat: Add event listener to get  player name from form

--- src/index.js
+++ src/index.js
@@ -9,3 +9,13 @@ dom.menuEventHandler();
 dom.userBoardEventListener();
 dom.renderBoard(dom.AI, '.ai');
 dom.userOnAIBoardEventListener();
+
+const playerform = document.querySelector('#playerForm');
+playerform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    dom.user.name =
+        e.target.name.value.charAt(0).toUpperCase() +
+        e.target.name.value.slice(1).toLowerCase();
+    playerform.closest('#player-name-modal').remove();
+    document.querySelector('.modal-backdrop').remove();
+});
